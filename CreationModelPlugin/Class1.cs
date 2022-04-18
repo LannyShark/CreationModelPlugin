1dcfffa80d8809ba1dafe4f88db02b1d7845bd23
Илья Пермиловский <lannyshark@ya.ru>
2022-04-18 22:59:08 +0300

    Попытка добавить крышу

--- CreationModelPlugin/Class1.cs
+++ CreationModelPlugin/Class1.cs
@@ -1,4 +1,5 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
@@ -28,9 +29,79 @@ namespace CreationModelPlugin
 
             AddDoor(doc, listLevel, walls[0]);
             AddWindows(doc, listLevel, walls);
-
+            AddRoof(doc, listLevel, walls);
 
             return Result.Succeeded;
+        }
+
+        private void AddRoof(Document doc, List<Level> listLevel, List<Wall> walls)
+        {
+            var level = listLevel
+                .Where(x => x.Name.Equals("Уровень 2"))
+                .FirstOrDefault();
+
+            RoofType roofType = new FilteredElementCollector(doc)
+                .OfClass(typeof(RoofType))
+                .OfType<RoofType>()
+                .Where(x => x.Name.Equals("Типовой - 400мм"))
+                .Where(x => x.FamilyName.Equals("Базовая крыша"))
+                .FirstOrDefault();
+
+            double wallWidth = walls[0].Width;
+            double df = wallWidth / 2;
+            double dh = level.get_Parameter(BuiltInParameter.LEVEL_ELEV).AsDouble();
+            XYZ dt = new XYZ(-df, -df, dh);
+            XYZ dz = new XYZ(0, 0, 20);
+            XYZ dy = new XYZ(0, 20, 0);
+            LocationCurve locationCurve = walls[0].Location as LocationCurve;
+            XYZ point = locationCurve.Curve.GetEndPoint(0);
+            XYZ p1 = point + dt;
+            XYZ p2 = p1 + dz;
+            XYZ p3 = p1 + dy;
+            XYZ origin = point;
+            XYZ vx = XYZ.BasisY;
+            XYZ vy = XYZ.BasisZ;
+            //List<XYZ> points = new List<XYZ>();
+            //points.Add(new XYZ(-dt, -dt, 0));
+            //points.Add(new XYZ(dt, -dt, 0));
+            //points.Add(new XYZ(dt, dt, 0));
+            //points.Add(new XYZ(-dt, dt, 0));
+            //points.Add(new XYZ(-dt, -dt, 0));
+
+            //Application application = doc.Application;
+            //CurveArray footprint = application.Create.NewCurveArray();
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    LocationCurve curve = walls[i].Location as LocationCurve;
+            //    XYZ p1 = curve.Curve.GetEndPoint(0);
+            //    XYZ p2 = curve.Curve.GetEndPoint(1);
+            //    Line line = Line.CreateBound(p1 + points[i], p2 + points[i+1]);
+            //    footprint.Append(line);
+            //}
+
+            CurveArray curve = new CurveArray();
+            curve.Append(Line.CreateBound(p1, p1 + new XYZ(0, 20, 20)));
+            curve.Append(Line.CreateBound(p1 + new XYZ(0, 20, 20), p1 + new XYZ(0, 40, 0)));
+
+
+            var av = doc.ActiveView;
+            Transaction transaction = new Transaction(doc, "Создание крыши");
+            transaction.Start();
+
+            ReferencePlane plane = doc.Create.NewReferencePlane(origin, origin + vx, origin + vy, av);
+
+            ExtrusionRoof extrusionRoof = doc.Create.NewExtrusionRoof(curve, plane, level, roofType, 0, 3);
+
+            //ModelCurveArray footPrintToModelCurveMapping = new ModelCurveArray();
+            //FootPrintRoof footprintRoof = doc.Create.NewFootPrintRoof(footprint, level, roofType, out footPrintToModelCurveMapping);
+            //foreach (ModelCurve modelCurve in footPrintToModelCurveMapping)
+            //{
+            //    footprintRoof.set_DefinesSlope(modelCurve, true);
+            //    footprintRoof.set_SlopeAngle(modelCurve, 0.5);
+            //}
+            transaction.Commit();
+
         }
 
         private void AddWindows(Document doc, List<Level> listLevel, List<Wall> walls)
